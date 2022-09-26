1063b163ca2b46fb19af95e5ebc62729a8730bbc
Replit user <>
2022-09-26 00:53:04 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -49,7 +49,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
 TreeNode * minimum(TreeNode * x){
   if(x->left == NULL){
-    return NULL;
+    return x;
   }
   else{
     while (x->left != NULL){
